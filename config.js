2be00eb05595738734a375405d71bb20fbd50989
hihozhou <hihozhou@gmail.com>
2018-12-05 14:33:02 +0800

    小修改

--- config.js
+++ config.js
@@ -1,22 +1,24 @@
-  // page title = baseTile + content title
+// page title = baseTile + content title
 config.baseTitle = 'Go Web 编程'
 config.siteName = '《Go Web 编程》'
 config.siteDes = 'Go Web 编程的描述信息'
 config.siteKey = 'go-web-dev'
 config.logoUrl = location.pathname
 
-config.lang = 'zh'
-config.langs = ['en', 'ja', 'pt-br', 'zh']
+config.lang = 'zh-CN'
+config.langs = ['en', 'zh-CN']
 
-config.docProject = 'astaxie/build-web-application-with-golang'
-config.project = 'astaxie/build-web-application-with-golang'
+config.docProject = 'phpopencv/docs'
+config.project = 'hihozhou/php-opencv'
 
-config.authorPage = 'https://github.com/astaxie'
-config.authorName = 'astaxie'
+config.authorPage = 'https://github.com/hihozhou'
+config.authorName = 'hihozhou'
 
-config.theme = 'paper'
+config.theme = 'flatly'
 config.codeTheme = 'github'
 config.catelogPage = 'SUMMARY.md'
 config.defaultPage = 'README.md'
 config.makeTOC = true
 config.emptyData = 'No content to display!'
+
+// config.dataUrl = 'http://0.0.0.0:8088/doc/{lang}/'
